52dbd75ee16e05e4dc28fbe40955fb5d7cb57adb
Francesc Busquets <francesc@gmail.com>
2025-03-25 17:30:46 +0100

    Update readme

--- src/components/Ajuda.js
+++ src/components/Ajuda.js
@@ -12,34 +12,34 @@ function Ajuda() {
       </p>
       <p>
         El vocabulari es pot consultar per ordre alfabètic o estructurat en diferents camps semàntics
-        o classificacions, on aquests, a la vegada s'estructuren en famílies.
+        o classificacions. Aquestes, a la vegada, s'estructuren en famílies.
       </p>
       <p className="bold">
         A cada paraula hi trobareu:
       </p>
       <ul>
-        <li>Un o més vídeos de la paraula representat en el llenguatge dels signes.</li>
+        <li>Un o més vídeos de la paraula representada en el llenguatge de signes català.</li>
         <li>Un dibuix de la paraula</li>
-        <li>Un so (veu) de la paraula</li>
+        <li>El so amb veu de la paraula</li>
       </ul>
       <p className="bold">
         Està destinat a:
       </p>
       <ul>
         <li>Nenes i nens que utilitzen els signes manuals com a suport de comunicació.</li>
-        <li>Nenes i nens sords que necessitin aprendre la Llengua de Signes.</li>
+        <li>Nenes i nens sords que necessitin aprendre la llengua de signes.</li>
         <li>Professorat d'EE i logopedes.</li>
         <li>Pares, mares i tutors.</li>
       </ul>
       <h2>Cercar una paraula</h2>
       <p>
-        En primer lloc s'haurà de seleccionar la classificació que desitja com a criteri de cerca.
-        En el cas que es vulgui cercar en totes les paraules del diccionari, és seleccionarà l'opció
+        En primer lloc, haureu de seleccionar la classificació que desitgeu com a criteri de cerca.
+        En cas que vulgueu cercar a totes les paraules del diccionari, deixeu seleccionada l'opció
         "TOTES LES CLASSIFICACIONS".
       </p>
       <p>
-        A continuació es farà el mateix amb les famílies, es seleccionarà la família desitjada per
-        concretar els criteris de cerca.
+        A continuació es farà el mateix amb les famílies, seleccionant la família desitjada per
+        tal de concretar els criteris de cerca.
       </p>
       <p>
         Quan s'escrigui la primera lletra en el quadre de cerca, automàticament es posicionarà en la
@@ -47,49 +47,84 @@ function Ajuda() {
         les lletres que contingui. Així si per exemple quan s'escrigui la lletra "A", el sistema es
         posicionarà en la primera paraula que contingui una "A", si continuem escrivint "AS" es
         posicionarà en la primera paraula que contingui "AS", i així fins que escrivim tota la paraula o
-        si la paraula que s'ha seleccionat es la que cercàvem. Fent clic en la paraula o prement la tecla ENTER
-        ens mostrarà la informació de la paraula.
+        fins que trobem la paraula que cercàvem. Fent clic a la paraula o prement la tecla ENTER
+        es mostrarà la informació associada a la paraula escollida.
       </p>
       <p>
         També es pot seleccionar la paraula directament de la llista de paraules, polsant amb el ratolí sobre
-        la paraula o desplaçant-se amb el cursos del teclat.
+        la paraula o desplaçant-se amb el cursor mitjançant el teclat.
       </p>
       <p>
-        Un cop s'ha seleccionat una paraula apareixerà en pantalla:
+        Un cop hàgiu seleccionat una paraula, apareixerà en pantalla:
       </p>
       <ul>
         <li className="bold">La paraula.</li>
         <li>
-          <span className="bold">El vídeo de la paraula representada en llenguatge de signes.</span>
-          En cas que existeixi mes d'un vídeo per aquella paraula, apareixerà una llista a sota del
-          vídeo mostrant les variants disponibles.
+          <span className="bold">El vídeo de la paraula representada en llenguatge de signes.</span> En
+          cas que existeixi més d'un vídeo per a aquella paraula, apareixerà una llista mostrant les variants disponibles.
         </li>
         <li className="bold">Una imatge de la paraula.</li>
         <li>
-          <span className="bold">El So de la paraula parlada</span>, que podrem tornar a escoltar polsant el botó "Torna a dir-ho".
+          <span className="bold">El so de la paraula parlada</span>, que podrem tornar a escoltar polsant el botó "Torna a dir-ho".
         </li>
       </ul>
       <h2>Treballar en modalitat "Recordem"</h2>
       <p>
-        Mira Què Dic té dues modalitats de treball:
+        El "Mira Què Dic!" té dues modalitats de treball:
       </p>
       <ul>
         <li>
-          La modalitat <span className="bold">Diccionari</span> que és amb la que inicia el programa. Permet consultar paraules
-          mostrant tota la informació multimèdia.
+          La modalitat <span className="bold">Diccionari</span>, que permet consultar paraules
+          mostrant tota la informació multimèdia que tinguin associada.
         </li>
         <li>
-          La modalitat <span className="bold">Recordem</span>. La funcionalitat d'aquesta modalitat és avaluar a l'alumne. Mostra només
-          el vídeo de la paraula i l'alumne ha d'esbrinar de quina paraula es tracta. També pot donar pistes a l'alumne per ajudar-lo
-          a esbrinar la paraula, activant el text, el so i/o la imatge polsant sobre els botons que apareixen a sobre del elements que
-          volem mostrar.
+          La modalitat <span className="bold">Recordem</span>, que planteja a l'alumne el repte d'endevinar una paraula a partir de la
+          seva representació en llenguatge de signes. La paraula pot sorgir a l'atzar, fent clic en el botó que mostra un dau, o bé
+          pot ser escrita per l'educador. En qualsevol dels dos casos, només veurem el nombre de caràcters que formen la paraula, que queda
+          amagada com si fos una contrasenya, i un vídeo amb la seva representació en llenguatge de signes. L'alumne/a ha d'endevinar la paraula
+          amagada. Si no la coneix, pot demanar fins a tres pistes: el so, la imatge o el text. Quan estigui segur d'haver-la encertat,
+          l'ha d'escriure a la caixa de text i fer clic al botó "Comprova-ho".
         </li>
       </ul>
+      <h2>Instal·lació de l'aplicació</h2>
+      <p>
+        El "Mira què dic!" es pot utilitzar directament des de la web i també es pot instal·lar com a aplicació independent en ordinadors,
+        tauletes i dispositius mòbils.
+      </p>
+      <p>
+        Els principals avantatges d'instal·lar-lo com a aplicació independent són:
+      </p>
+      <ul>
+        <li>L'aplicació <span className="bold">s'engega més de pressa</span>, directament a partir d'una icona al mòbil o al menú "Inici" de l'ordinador.</li>
+        <li>S'eliminen <span className="bold">elements distractors</span> com ara els menús, botons i barres del navegador web: l'aplicació ocupa tota la pantalla,
+          aprofitant al màxim l'espai dels dispositius mòbils.</li>
+        <li>Ocasionalment, l'aplicació <span className="bold">pot funcionar sense connexió a Internet</span>. Els vídeos, imatges i sons de les darreres 100 paraules
+          vistes es conserven al dispositiu per poder seguir treballant-les quan es trobi fora de línia.</li>
+      </ul>
+      <p>
+        Per instal·lar l'aplicació al vostre dispositiu només cal que visiteu la <a href="https://mqdic.edigital.cat/">pàgina principal</a> amb
+        el navegador web i seguiu les instruccions següents:
+      </p>
+      <ul>
+        <li>
+          En sistemes <span className="bold">Android</span>, <span className="bold">Windows</span> i <span className="bold">Linux</span>: feu clic al botó "Instal·la l'aplicació" que surt a la pantalla d'inici. Us apareixerà una finestra
+          de diàleg on us preguntaran si voleu fer la instal·lació. Confirmeu-ho i tot seguit disposareu de la icona per engegar-la a l'escriptori
+          o al menú d'inici.
+        </li>
+        <li>
+          En sistemes <span className="bold">Apple iOS</span> (iPhone i iPad) heu d'utilitzar el navegador Safari per visitar la pàgina principal (no funciona amb altres navegadors).
+          Feu clic al botó "Comparteix" que hi ha a la part inferior de la pantalla i, tot seguit, seleccioneu l'opció
+          "Envia a l'escriptori" per tal que aparegui la icona de l'aplicació a la pantalla del mòbil o la tauleta.
+        </li>
+      </ul>
+      <p>
+        L'aplicació es pot desinstal·lar en qualsevol moment des del gestor de programari de cada sistema operatiu, com un programa més.
+      </p>
       <h2>Vegeu també...</h2>
       <p>
-        La llista de totes les paraules incloses a l'aplicació, així com la relació completa d'imatges, sons i vídeos es pot consultar 
+        La llista de totes les paraules incloses en l'aplicació, així com la relació completa d'imatges, sons i vídeos es pot consultar
         en <a href="https://docs.google.com/spreadsheets/d/1lKUyOrmFfTzWKyLfTNXI4sLX2d-e9ONCyMBFGGi9zxY/edit?usp=sharing" target="_blank">aquest
-        document en línia</a>.
+          document en línia</a>.
       </p>
       <p>
         L'aplicació <span className="bold">Mira Què Dic</span> és un projecte de programari lliure distribuït sota els termes de
